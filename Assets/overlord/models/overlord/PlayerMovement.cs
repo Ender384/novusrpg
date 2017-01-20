4e3e49f1bae3c5d5f54f8382543f3922e93d11aa
Ender384 <ender384@live.com>
2017-01-19 21:08:05 -0600

    test

---
+++ Assets/overlord/models/overlord/PlayerMovement.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class PlayerMovement : MonoBehaviour {
+    public Animator anim;
+
+
+
+	void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+
+
+    void Update()
+    {     
+        if (Input.GetButton("Shift"))
+        {
+            anim.SetBool("Shift", true);
+        }
+        else
+            anim.SetBool("Shift", false);
+        if (Input.GetButton("Fire1"))
+        {
+            anim.SetBool("Attack", true);
+        }
+        else
+            anim.SetBool("Attack", false);
+        Move();
+    }
+
+
+
+    void Move()
+    {
+        anim.SetFloat("Forward", Input.GetAxis("Vertical"));
+    }
+}
